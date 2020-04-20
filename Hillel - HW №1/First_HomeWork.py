1cef0d99b9f88288116c6bd69d4bacd8ce87e701
DaniilNesterenko <daniil.nesterenko77@gmail.com>
2020-04-20 22:47:10 +0300

    First HW for Hillel School

---
+++ Hillel - HW №1/First_HomeWork.py
@@ -0,0 +1,10 @@
+# Home-work №1
+# Task №1 ( Write in the console own name by using of *,spaces and ESCAPE symbols like \n and \t )
+# Below can be seen a code which displays my name: Daniil
+print('* * *   \t\t*   \t*   \t*   *   *   * \t\n*\t  *\t\t   * *\t\t**\t    *   \t\t* \t\n*\t   *\t  *   * \t* * \t*   *   *   * \t\n*\t\t*\t *     *    *  *    *   *   *   * \t\n*       *   *********   *   *   *   *   *   * \t\n*   \t*   *   \t*   *    *  *   *   *   * \t\n*\t    *   *   \t*   *     * *   *   *   * \t\n*     *  \t*   \t*   *\t   **   *   *   * \t\n* * *   \t*   \t*   *   \t*   *   *   * * * * * *')
+
+
+
+# Task №2 ( Write a programme which displays in the console table of Escape sequences )
+# Below can be seen a code which displays in the console table of Escape sequences
+print("Escape sequences \n\\a\tBell (alert)\n\\b\tBackspace\n\\n\tNew line\n\\t\tHorizontal tab\n\\\ \tBackslash \\\n\\\"\tDouble quotation mark \"\n\\'\tSingle quotation mark '")
